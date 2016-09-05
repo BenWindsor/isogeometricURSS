c0233477e01e06cc44fcf138eae332de110aac3c
Ben Windsor <B.Windsor@warwick.ac.uk>
2016-09-05 21:40:38 +0100

    Curve shortening attempt and EOC attempt files

---
+++ curveShorteningAttempt2.m
@@ -0,0 +1,60 @@
+%Curve shortening flow of the unit circle with no forcing term SINGLE STEP
+
+% Set up fixed surface parameters
+xHandle=@(x)(cos(2*pi*x));
+yHandle=@(x)(sin(2*pi*x));
+degree=2;
+elemNum=15;
+
+% Create surface and load geometry
+crv=periodicCurveInterpolate(elemNum, degree, xHandle, yHandle);
+geometry=geo_load(crv);
+knots=geometry.perbspline.knots;
+
+% Create mesh
+[qn, qw] = msh_set_quad_nodes(knots, msh_gauss_nodes(crv.order));
+msh=msh_cartesian(knots, qn, qw, geometry);
+
+% Create discrete space of periodic basis splines
+space=sp_perbsp(geometry.perbspline, msh);
+
+% Set time step
+delta=0.05;
+
+M = op_u_v_tp(space, space, msh); %mu = 1
+M=M(1:elemNum, 1:elemNum);
+A = op_gradu_gradv_tp(space, space, msh); %epsilon = 1
+A=A(1:elemNum, 1:elemNum);
+mat = (1/delta)*(M)+A;
+
+
+prevxCoefs=crv.coefs(1,:);
+prevyCoefs=crv.coefs(2,:);
+
+steps=5;
+
+newxCoefs=zeros(steps, elemNum);
+newyCoefs=zeros(steps, elemNum);
+
+for i=1:steps
+    rhs1=(1/delta)*M*prevxCoefs' + 0; %+0 for forcing term 
+    rhs2=(1/delta)*M*prevyCoefs' + 0;
+
+    newxCoefs(i,:)=mat\rhs1;
+    newyCoefs(i,:)=mat\rhs2;
+    
+    prevxCoefs=newxCoefs(i,:);
+    prevyCoefs=newyCoefs(i,:);
+end
+
+
+newCrv=perbspmak(newCoefs, knots);
+
+hold on;
+
+for i=1:steps
+    newCoefs=[newxCoefs(i,:); newyCoefs(i,:)];
+    newCrv=perbspmak(newCoefs, knots);
+    perbspplot(crv, 100);
+    perbspplot(newCrv, 100);
+end
